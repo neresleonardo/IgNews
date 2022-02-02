1d85ed5ddbf6d04a49a2ada5739225c9cc02d320
Leonardo Borges <leonardo.borges.neres@gmail.com>
2022-02-02 14:19:04 -0300

    Componente: Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import styles from './styles.module.scss'
+
+export function Header() {
+    return(
+        <header className={styles.heandleContainer}>
+            <div className={styles.heandleContent}>
+
+                <img src="/images/logo.svg" alt="LogoImg" />
+                <nav>
+                <a className={styles.active} href="#">Home</a>  
+                <a href="#">Posts</a> 
+
+                </nav>
+            </div>
+        </header>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
-import type { NextPage } from 'next'
+import Head from 'next/head'
 
-const Home: NextPage = () => {
-  return (
+export default function Home(){
+  return(
+    <> 
+     <Head>
+        <title>Inicio</title>
+      </Head>
     <h1>Home</h1>
+    </>
   )
 }
-
-export default Home
