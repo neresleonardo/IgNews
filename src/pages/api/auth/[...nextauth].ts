c77709e9bdc7189bc9b5fb076758f19ee274d92e
Leonardo Borges <leonardo.borges.neres@gmail.com>
2022-02-03 20:39:30 -0300

    Saving user in the bank

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from 'faunadb'
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -15,4 +18,27 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile}) {
+      
+      const { email} = user 
+
+        try {
+
+          await fauna.query(
+            q.Create(
+              q.Collection('users'),
+              { data: {email}}
+            )
+          )
+  
+          return true
+          
+        } catch (err) {
+          console.log(err)
+          return false
+        }
+    }
+  }
+
 })
