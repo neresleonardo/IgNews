b1425d7fdfad9a4857f8e185500383d7a8149daa
Leonardo Borges <leonardo.borges.neres@gmail.com>
2022-02-03 17:46:24 -0300

    Configuring FaunaDB

---
+++ .history/src/components/signInButton/index_20220203134820.tsx
@@ -0,0 +1,33 @@
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+import styles from './styles.module.scss'
+import {signOut, signIn, useSession } from 'next-auth/react'
+
+export function SignInButton() {
+
+    // Icon color
+    const { data: session } = useSession()
+
+    console.log(session);
+    
+
+    return session ? (
+
+        <button type="button" onClick={() =>signOut()} className={styles.signInButton}>
+            <img src={session.user?.image} alt="" />
+            {session.user?.name}
+            <FiX color="#737380" className={styles.claseIcon} />
+        </button> 
+
+    ) : (
+
+        <button type="button" onClick={() => signIn('github')} className={styles.signInButton}>
+            <FaGithub color="#eba417" />
+            Sing in with GitHub  
+        </button>  
+
+    );
+}
+
+
+
