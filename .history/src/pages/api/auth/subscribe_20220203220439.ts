5f398aa5081bdead9a09a461519d8ba0945004d0
Leonardo Borges <leonardo.borges.neres@gmail.com>
2022-02-04 17:15:21 -0300

    Listening to Stripe Events

--- .history/src/pages/api/auth/subscribe_20220203220439.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse, NextRequest } from "next/server";
-
-export default async (req: NextRequest, res: NextResponse) => {
-    if (req.method === "POST"){
-
-    } else {
-        res.setHeader("Allow", 'POST')
-    }
-}
--- .history/src/pages/api/auth/subscribe_20220203220814.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async (req: NextApiRequest, res: NextApiResponse, res: ) => {
-    if (req.method === "POST"){
-
-    } else {
-        res.setHeader('')
-    }
-}""
