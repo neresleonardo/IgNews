a65d704a0d060034dd71595362c09afbf5f9c01e
Leonardo Borges <leonardo.borges.neres@gmail.com>
2022-02-07 10:19:31 -0300

    problema com o <any>

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -4,6 +4,18 @@ import Head from 'next/head';
 import { getPrismicClient } from '../../services/prismic';
 import styles from './home.module.scss'
 import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
+
+type Post = {
+    slug: string;
+    title: string;
+    excerpt: string;
+    updated: string;
+}
+
+interface PostsProps{
+    posts: Post[];
+}
 
 export default function Posts() {
     return(
@@ -52,7 +64,7 @@ export default function Posts() {
 export const getStaticProps: GetStaticProps = async () => {
     const prismic = getPrismicClient()
 
-    const response = await prismic.query([
+    const response = await prismic.query<any>([
         Prismic.predicates.at('document.type', 'Publication')
     ], {
        fetch: ['Publication.title', 'Publication.content'],
@@ -61,8 +73,22 @@ export const getStaticProps: GetStaticProps = async () => {
 
     console.log(response);
     
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date!).toLocaleDateString('pt-BR',{
+                day:'2-digit',
+                month:'long',
+                year:'numeric'
+            })
+            };
+        });
     
     return {
-        props: {}
+        props:{posts}
+ 
+ 
     }
-}
+} 
