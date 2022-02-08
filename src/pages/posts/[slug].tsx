7fe7515be4fd06a8bd42d03cf8647beb79bf8e62
Leonardo Borges <leonardo.borges.neres@gmail.com>
2022-02-08 13:39:17 -0300

    Validating active subscription

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -35,28 +35,43 @@ export default function Post({ post }: PostProps){
     )
 }
 
-export const getServerSideProps: GetServerSideProps = async ({req, params}) => {
-    const session = await getSession({ req })
+export const getServerSideProps: GetServerSideProps = async ({
+    req,
+    params,
+  }) => {
+    const session = await getSession({ req });
     const { slug } = params!;
-
-    const prismic = getPrismicClient(req)
-
-    const response = await prismic.getByUID<any>('post', String(slug), {})
-
+  
+    if (!session?.activeSubscription) {
+      return {
+        redirect: {
+          destination: '/',
+          permanent: false,
+        },
+      };
+    }
+  
+    const prismic = getPrismicClient(req);
+  
+    const response = await prismic.getByUID<any>('post', String(slug), {});
+  
     const post = {
-        slug,
-        title: RichText.asText(response.data.title),
-        content: RichText.asHtml(response.data.content),
-        updatedAt: new Date(response.last_publication_date!).toLocaleDateString('pt-BR', {
-            day: '2-digit',
-            month: 'long',
-            year: 'numeric'
-        })
-    }
-
-    return {
-        props: {
-            post
+      slug,
+      title: RichText.asText(response.data.title),
+      content: RichText.asHtml(response.data.content),
+      updatedAt: new Date(response.last_publication_date!).toLocaleDateString(
+        'pt-BR',
+        {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
         }
-    }
-}
+      ),
+    };
+  
+    return {
+      props: {
+        post,
+      },
+    };
+  };
